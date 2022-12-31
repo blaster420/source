c6952847ee78bb81d34377975d990369aa9fcf84
Blaster <109479382+blaster420@users.noreply.github.com>
2022-12-31 01:58:52 -0500

    Add files via upload

--- sourceveiwer.lua
+++ sourceveiwer.lua
@@ -1,7 +1,6 @@
-
 _G.enable = false
 _G.color = Color3.fromRGB(255,0,0)
-_G.toggle_keybind = "i" -- enable tracer and disable
+_G.toggle_keybind = "l" -- enable tracer and disable
 _G.swith_nigga = 't' -- press t and u will see a noti on the user ur tracer is on 
 _G.method = "MousePos" --had a stroke sotkraakdakdakdkadkadkakdakdakdkdakdakdkadkadka
 
